c1bf3e93ef41db990c3b8bc7ad9fc98ae63c4f06
duwscan <109639466+duwscan@users.noreply.github.com>
2023-10-23 22:36:28 +0700

    create base Example of fragment

--- app/src/main/java/com/app/todoapp/MainActivity.java
+++ app/src/main/java/com/app/todoapp/MainActivity.java
@@ -1,13 +1,51 @@
 package com.app.todoapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.app.todoapp.databinding.ActivityMainBinding;
+import com.app.todoapp.fragments.IndexFragment;
 
 public class MainActivity extends AppCompatActivity {
+    final static int INDEX_FRAGMENT = R.id.index;
+    final static int CALENDAR_FRAGMENT = R.id.calendar;
+    final static int FOCUS_FRAGMENT = R.id.focuse;
+    final static int SETTINGS_FRAGMENT = R.id.settings;
+    private ActivityMainBinding binding;
+    private TextView titleHeader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        // initial fragment
+        titleHeader = binding.headerTitle;
+        MenuItem initialFragment = binding.bottomNavigationView.getMenu().getItem(0);
+        titleHeader.setText(initialFragment.getTitle());
+        replaceFragment(new IndexFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            titleHeader.setText(item.getTitle());
+            if (itemId == INDEX_FRAGMENT) {
+                replaceFragment(new IndexFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_content, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/app/todoapp/fragments/IndexFragment.java
@@ -0,0 +1,22 @@
+package com.app.todoapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.app.todoapp.databinding.FragmentIndexBinding;
+public class IndexFragment extends Fragment {
+    FragmentIndexBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentIndexBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+}
